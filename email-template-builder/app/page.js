ca5d30b8629f032f7427179ece52b1c9349d7276
TechBingeWorld <ahmad.arman.deos@gmail.com>
2025-01-23 02:20:23 +0530

    project setup

---
+++ email-template-builder/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="">
+      <h1>Email Template Builder</h1>
+      <Button>Generate</Button>
+    </div>
+  );
+};
+
+export default Home;
